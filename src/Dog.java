7e7a3920a31efe2ac3178a340e967b3927faafd6
Maks <makzylinski@gmail.com>
2024-07-31 13:23:35 +0200

    Overriding methods

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,36 @@
 public class Dog extends Animal {
 
+    private String earShape;
+    private String tailShape;
     public Dog() {
-        super();
+        super("Mutt", "Big", 50);
+    }
+
+    public Dog(String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight );
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+    public void makeNoise() {       // Overriding method makeNoise(), it has the same signature as Animal makeNoise() method, but it's overwritten here, since Dog class extends Animal class
+
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs walk, run and wag their tail");
     }
 }
